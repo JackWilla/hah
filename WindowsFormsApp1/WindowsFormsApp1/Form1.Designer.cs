bf9d3e82fb0f663826bbd26682606c8386759b1a
Your Name <you@example.com>
2018-12-19 13:39:25 +0300

    add action for button

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,16 +30,18 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(601, 71);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(144, 48);
             this.button1.TabIndex = 0;
             this.button1.Text = "Поиск";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // comboBox1
             // 
@@ -48,22 +50,33 @@
             "футбол",
             "теннис",
             "волейбол"});
-            this.comboBox1.Location = new System.Drawing.Point(397, 71);
+            this.comboBox1.Location = new System.Drawing.Point(12, 66);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(144, 21);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +84,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,11 +15,28 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            label1.Text = null;
         }
 
         private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "футбол")
+            {
+                label1.Text = "Все группы сформированы.\nДля точной информации позврните по номеру +7**********";
+            }
+            if (comboBox1.Text == "теннис")
+            {
+                label1.Text = "Есть запись в 3 группы.\n\nГруппа №1 занимается на станции метро ***\nТренер: Петров Петр Петрович\nТелефон: +7**********\n\nГруппа №2 занимается на станции метро *****\nТренер: Николаенкова Олеся Игоревна\nТелефон: +7**********\n\nГруппа №3 занимается на станции метро *******\nТренер: Курягин Кучер Веренский\nТелефон: +7**********";
+            }
+            if (comboBox1.Text == "волейбол")
+            {
+                label1.Text = "Есть запись в 1 группу.\n\nГруппа №1 занимается на станции метро *****\nТренер: Северова Виктория Сергеевна\nТелефон: +7**********";
+            }
         }
     }
 }
